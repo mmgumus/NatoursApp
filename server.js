3d127b837149efbc4f8212506edae87d3c80665a
Muhammet Mahmut Gümüş <56482835+mmgumus@users.noreply.github.com>
2023-03-28 18:58:09 +0300

    server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require("dotenv");
 const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+console.log(proccess.env);
 
 const port = 3000;
 app.listen(port, () => {
-	console.log("App running on port ${port}...");
+  console.log("App running on port ${port}...");
 });
