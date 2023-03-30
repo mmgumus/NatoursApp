990308373d27fb9a072d97a8ff1e7480eac8db8f
mmgumus <mahmut_muhammet@hotmail.com>
2023-03-30 05:37:15 +0300

    atlas database bağlantısı yapıldı, mongoose yüklen

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+const app = require('./app');
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("App running on port ${port}...");
+  console.log('App running on port ${port}...');
 });
